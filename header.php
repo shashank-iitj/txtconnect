c2b38c8fcc58c817adcfc9a9365f861ddec267b1
Sumit Sharma <sumit@iitj.ac.in>
2012-08-07 13:08:00 +0530

    improvement in gui

--- header.php
+++ header.php
@@ -1,10 +1,11 @@
 <html>
-		<table bgcolor="d8d8d8" align="center" border="2" width="400">
+	<div class='logo'>		
+		<table align="center" border="2" width="400">
 			<TR>
 				<TD align="center">
 					<font size='10' >Text Connect</font>
 				</TD>
 			</TR>
 		</table>
-	<hr>
+	</div>
 </html>
--- upper.php
+++ upper.php
@@ -1,12 +1,9 @@
 <html>
-	<table align='center' width='100%' bgcolor='f0f0f0'>
-		<tr>
-			<td align='center'>
-				<a href='home.php'>Home</a>
-			</td>
-			<td align='center'>
-				<a href='logout.php'>Logout</a>
-			</td>
-		</tr>
-	</table>
+	<div class='nav'>
+		<ul>
+				<li ><a href='home.php'>Home</a></li>
+				<li ><a href='logout.php'>Logout</a></li>
+		</ul>
+	</div>
+	<br>
 </html>
